adf0c0c729cfaebf97ea31200b56bf2d4bae08c7
OscarLoZa05 <oscarlzafra@gmail.com>
2026-03-26 13:36:14 +0100

    Teleport

---
+++ Assets/Scripts/Props/Portal/Portal.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Teleport");
+        }
+    }
+}
